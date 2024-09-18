d3824df71e669391f3b99e6616dd9cfbdac34c09
Binita Kumari <binita.banti@gmail.com>
2024-09-18 19:21:36 +0530

    created a controller and a notes files

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    //POST[name, email, password]
+    public function register(Request $request)
+    {
+
+    }
+
+    //POST[email, password]
+    public function login(Request $request)
+    {
+
+    }
+    //GET[Auth : Token]
+    public function profile()
+    {
+
+    }
+
+    //GET[Auth:Token]
+    public function logout()
+    {
+
+    }
+
+
+}
